2c43c3ba3c0c8a700c6ea463fda1c8d1a795507d
german <gg@mail.ru>
2026-03-03 18:39:20 +0300

    Добавил остаток от деления, умножение, выведение округлённого числа

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,45 @@
     }
     public class Calculator
     {
-        
+        public double Adding(double num1,double num2) => num1 + num2;
+        public double Mult(double num1, double num2) => num1 * num2;
+        public void PrintOkruglenii(int num,int NumOfF)
+        {
+            switch (num) 
+            {
+                case 1:
+                    {
+                        Console.WriteLine($"{num:1F}");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine($"{num:2F}");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine($"{num:3F}");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine($"{num:4F}");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine($"{num:5F}");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine($"{num:6F}");
+                        break;
+                    }
+            }
+
+        }
+        public double OstatokOtDeleniya(double num1 , double num2)  => num1 % num2;
     }
 }
