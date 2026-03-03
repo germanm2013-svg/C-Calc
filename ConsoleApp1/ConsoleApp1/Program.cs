459a8e52af4c6b6e5bd38d8bf9a43f31019ae804
german <gg@mail.ru>
2026-03-03 18:23:06 +0300

    Interface

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите пример");
+            Console.WriteLine("*n1 (действие) n2*");
+        }
+
+    }
+    public class Calculator
+    {
+        
+    }
+}
