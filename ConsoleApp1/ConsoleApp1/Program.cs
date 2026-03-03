4010717d47ed706766552704ab6c77fe50cbb6de
Makar <makaryourboss@gmail.com>
2026-03-03 18:38:14 +0300

    Измененный мэйн и - /

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,30 @@
         {
             Console.WriteLine("введите пример");
             Console.WriteLine("*n1 (действие) n2*");
+            double FirstNumber = Convert.ToInt32(Console.ReadLine());
+            double SecondNumber = Convert.ToInt32(Console.ReadLine());
+            string Znak = Console.ReadLine();
+            if (Znak == "-")
+            {
+                Calculator.Minus(FirstNumber, SecondNumber);
+            }
+            if (Znak == "/")
+            {
+                Calculator.Delit(FirstNumber, SecondNumber);
+            }
         }
 
     }
     public class Calculator
     {
-        
+        public static void Minus(double FirstNumber, double SecondNumber)
+        {
+            Console.WriteLine($"{FirstNumber} - {SecondNumber} = {FirstNumber - SecondNumber}");
+            
+        }
+        public static void Delit(double FirstNumber, double SecondNumber)
+        {
+            Console.WriteLine($"{FirstNumber} / {SecondNumber} = {FirstNumber / SecondNumber}");
+        }
     }
 }
